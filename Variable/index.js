664af347c5e06b6315098260352fce6c5f7916f5
Sutthatip Kanchanapradit <sutthatip4@gmail.com>
2021-06-29 00:11:34 +0700

    Variable

---
+++ Variable/index.js
@@ -0,0 +1,13 @@
+//var let const
+
+let fruit = "mango"
+let price = 20
+const person = {name:"four",last: "se"} //variable constant can change value when variable is object
+person.name = "super"
+if(price === 20){
+    let fruit = "melon"
+    console.log(fruit);
+}
+
+console.log(fruit);
+console.log(person);
